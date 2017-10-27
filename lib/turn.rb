ed766f5702a098b81553dd0af29ec5914110ddd6
Keith  Polischuk <kpolischuk@gmail.com>
2017-10-27 01:01:00 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,5 @@
+def display_board(cell)
+  display = " #{cell[0]} | #{cell[1]} | #{cell[2]} \n-----------\n #{cell[3]} | #{cell[4]} | #{cell[5]} \n-----------\n #{cell[6]} | #{cell[7]} | #{cell[8]} \n"
+puts display
+end
+
