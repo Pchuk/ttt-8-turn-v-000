2672b7e2fa2ca62fe3c5eb46ed85d44c338dd13b
Keith  Polischuk <kpolischuk@gmail.com>
2017-10-27 01:13:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,30 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+
+if position_taken?(board, index)
+  false
+elsif index.between?(0, 8)
+  true
+elsif index < 0
+    false
+  elsif index > 8
+    false
+
+end
+end
+
+def position_taken?(board, index)
+
+if board[index] == "X"
+  true
+elsif board[index] == "O"
+  true
+elsif board[index] == " " || "" || nil
+  false
+ end
+
+end
+
